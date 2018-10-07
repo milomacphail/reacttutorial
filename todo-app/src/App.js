b5a56b0b8d0de50ed1eaa00b95d336071671cd68
Milo MacPhail <milomacphail@gmail.com>
2018-10-07 17:54:35 +0000

    started todo app - allowed for deleting entries

---
+++ todo-app/src/App.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+import Todos from "./todos" 
+
+class App extends Component {
+  state = {
+    todos: [
+    {id: 1, content: "Buy some milk"},
+    {id: 2, content: "Play MarioKart"}
+    ]
+  }
+  deleteTodo = (id) => {
+    const todos = this.state.todos.filter(todo =>{
+      return todo.id !== id
+    });
+  this.setState({
+    todos
+  })
+  }
+  render() {
+    return (
+      <div className="todo-app container">
+      <h1 className = "center blue text">Todos</h1>
+      <Todos todos= {this.state.todos} deleteTodo = {this.deleteTodo}/>
+      </div>
+    );
+  }
+}
+
+export default App;
